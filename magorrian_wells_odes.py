3095781a57b40344d1756406f0dea25d4a12584a
indigo-fish <indigo-fish@users.noreply.github.com>
2021-07-16 17:06:12 +0100

    Add files via upload

--- magorrian_wells_odes.py
+++ magorrian_wells_odes.py
@@ -7,6 +7,7 @@ as practice for creating my own with frazil ice
 import numpy as np
 import math
 from scipy.integrate import odeint
+from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
 """
@@ -32,12 +33,13 @@ S_s = 0 #salt concentration in ice
 #I am choosing theta arbitrarily
 theta = 0.1
 sin_theta = math.sin(theta)
-S_a = .035 #ambient water salinity
-#I don't know rho_a, rho_l, T_a, S_i
-rho_a = 1.03
-rho_l = 1.02
+S_a = 35 #ambient water salinity
+#I don't know rho_a, rho_l, rho_s
+rho_a = 1.0236
+rho_l = 1.0236
+rho_s = 0.9168
+#T_a is supposed to vary
 T_a = 0
-S_i = 0.035
 
 """
 functions which produce various other (generally non-constant)
@@ -59,28 +61,20 @@ def get_a(y):
 
 def get_b(y):
     S = get_S(y)
-    print("S " + str(S))
     T_L_S = get_T_L(S)
-    print("T_L_S " + str(T_L_S))
     T = get_T(y)
-    print("T " + str(T))
     T_L_S_s = get_T_L(S_s)
     return St_m * (1 - c_s / L * (T_s - T_L_S) - St * c_l / L * (T - T_L_S_s))
 
 def get_c(y):
     T = get_T(y)
-    print("T " + str(T))
     T_L_S = get_T_L(get_S(y))
-    print("T_L_S " + str(T_L_S))
     return - St_m * St * c_l / L * (T - T_L_S)
 
 def get_M(y):
     a = get_a(y)
-    print("a " + str(a))
     b = get_b(y)
-    print("b " + str(b))
     c = get_c(y)
-    print("c " + str(c))
     return (math.sqrt(b ** 2 - 4 * a * c) - b) / (2 * a)
 
 def get_U(y):
@@ -92,23 +86,29 @@ def get_H(y):
 def get_del_rho(y):
     return rho_l * y[2] / y[0]
 
-def get_T_eff():
-    T_i = get_T_L(S_i)
-    print("T_i")
-    print(T_i)
+def get_T_i(y):
+    T_i, S_i = fsolve(solve_system, [get_T_L(S_a), S_a], args = (get_M(y), get_U(y), get_T(y), get_S(y)))
+    return T_i
+
+def get_S_i(y):
+    T_i, S_i = fsolve(solve_system, [get_T_L(S_a), S_a], args = (get_M(y), get_U(y), get_T(y), get_S(y)))
+    return S_i
+
+def get_T_eff(T_i):
     return T_i - L / c_l + c_s / c_l * (T_s - T_i)
 
-def get_rho_eff():
-    print("S part")
-    print(beta_s * (S_a - S_s))
-    print("T eff")
-    print(get_T_eff())
-    print("T part")
-    print(beta_T * (T_a - get_T_eff()))
-    return rho_l * (beta_s * (S_a - S_s) - beta_T * (T_a - get_T_eff()))
+def get_rho_eff(y):
+    return rho_l * (beta_s * (S_a - S_s) - beta_T * (T_a - get_T_eff(get_T_i(y))))
 
 def get_del_T_a():
     return T_a - get_T_L(S_a)
+
+#system of equations which is solved to find T_i and S_i, given M, U, T and S
+def solve_system(vect, M, U, T, S):
+    T_i, S_i = vect
+    func1 = rho_l * L * M * abs(U) + rho_s * c_s * (T_i - T_s) * M * abs(U) - rho_l * c_l * St * abs(U) * (T - T_i)
+    func2 = get_T_L(S_i) - T_i
+    return [func1, func2]
 
 """
 defines each of the linear differential equations
@@ -128,7 +128,7 @@ def dy1_ds(y):
 
 def dy2_ds(y):
     U = get_U(y)
-    del_rho_eff = get_rho_eff()
+    del_rho_eff = get_rho_eff(y)
     M = get_M(y)
     return del_rho_eff / rho_l * M * abs(U)
 
@@ -137,8 +137,8 @@ def dy3_ds(y):
     U = get_U(y)
     del_T_a = get_del_T_a()
     M = get_M(y)
-    T_eff = get_T_eff()
-    S_i = get_S(y)
+    T_eff = get_T_eff(get_T_i(y))
+    S_i = get_S_i(y)
     return del_T_a * E * abs(U) + M * abs(U) * (T_eff - T_m + tau * S_i) - lamda * sin_theta * y[0]
 
 """
@@ -154,20 +154,40 @@ def derivative(y, t):
 """
 comes up with initial values using analytical solutions
 """
+
+"""
+system of equations which is solved to find M, U, T, S, T_i and S_i
+for calculation of initial y values
+"""
+def solve_init_system(vect, E, X):
+    M, U, T, S, T_i, S_i = vect
+    func1 = beta_T * (T - T_a) - beta_s * (S - S_a) + M / (E + M) * (beta_s * (S_a - S_s) - beta_T * (T_a - get_T_eff(T_i)))
+    func2 = T + T_m + tau * (S - S_s) + ((T_a - get_T_L(S_a)) * E + (T_i - L / c_l + c_s / c_l * (T_s - T_i) - T_m + tau * (S - S_s)) * M) / (E + M)
+    func3 = - T_i + T_m - tau * (S - S_s)
+    func4, func5 = solve_system([T_i, S_i], M, U, T, S)
+    """
+    this is currently using absolute value inside square root
+    due to M being negative
+    """
+    func6 = - U + math.sqrt(2 * (E + M) / (3 * C_d + 4 * (E + M))) * math.sqrt(abs(M / (E + M) * (beta_s * (S_a - S_s) - beta_T * (T_a - get_T_eff(T_i))) * g * sin_theta * X))
+    return [func1, func2, func3, func4, func5, func6]
+
 X0 = 0.01
 E0 = E_0 * sin_theta
-M0 = .0005 * E0
+
+#solves analytic system of equations for M, U, T, S, T_i, S_i
+M0, U0, T0, S0, T_i0, S_i0 = fsolve(solve_init_system, [.0005 * E0, .01, T_a, S_a, 10000, S_a], args = (E0, X0))
+
+#converts solved-for values into initial values in y
 H0 = 2 / 3 * (E0 + M0) * X0
-del_T0 = (get_del_T_a() * E0 + get_T_eff() * M0) / (E0 + M0)
-del_rho0 = M0 / (E0 + M0) * get_rho_eff()
-print(del_rho0)
-print(get_rho_eff())
-U0 = math.sqrt(2 * (E0 + M0) / (3 * C_d + 4 * (E0 + M0))) * math.sqrt(abs(del_rho0) / rho_l * g * sin_theta * X0)
+del_T0 = T0 - get_T_L(S0)
+del_rho0 = - rho_l * (beta_s * (S0 - S_a) - beta_T * (T0 - T_a))
 
 y0_0 = H0 * U0
 y0_1 = H0 * U0 ** 2
 y0_2 = H0 * U0 * del_rho0 / rho_l
 y0_3 = H0 * U0 * del_T0
+
 #puts these initial values in a vector for solving equations
 y0 = [y0_0, y0_1, y0_2, y0_3]
 
@@ -175,5 +195,42 @@ y0 = [y0_0, y0_1, y0_2, y0_3]
 s = np.linspace(0, 100)
 y = odeint(derivative, y0, s)
 
+#functions for calculating analytic values at locations other than initial point
+def analytic_H(E, M, X):
+    return 2 / 3 * (E + M) * X
+
+"""
+again using absolute value in square root due to negative M
+"""
+def analytic_U(E, M, X):
+    return math.sqrt(2 * (E + M) / (3 * C_d + 4 * (E + M))) * math.sqrt(abs(M / (E + M) * (beta_s * (S_a - S_s) - beta_T * (T_a - get_T_eff(T_i0))) * g * sin_theta * X))
+
+def analytic_del_T(E, M, X):
+    return (get_del_T_a() * E + (T_i0 - get_T_L(S_s)) * M) / (E + M)
+
+def analytic_del_rho(E, M, X):
+    return M / (E + M) * rho_l * (beta_s * (S_a - S_s) - beta_T * (T_a - T_i0))
+
+#converts analytic functions into vector
+def analytic_values(X):
+    H = analytic_H(E0, M0, X)
+    U = analytic_U(E0, M0, X)
+    del_T = analytic_del_T(E0, M0, X)
+    del_rho = analytic_del_rho(E0, M0, X)
+    return [H, U, del_T, del_rho]
+
+#evaluates analytic functions at all points reported for differential equation solutions
+analytic_y = list(map(analytic_values, s))
+
+#plots first differential equations and second analytic solutions
+"""
+currently there is barely any agreement at all
+between the two solutions, which should agree
+"""
+plt.figure(1)
 plt.plot(s, y)
+plt.show()
+
+plt.figure(2)
+plt.plot(s, analytic_y)
 plt.show()
